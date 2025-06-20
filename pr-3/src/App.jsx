60179b69db41cf7a861466ab9d9ee2e1a58a5f82
Heney Goyani <bharatgoyani8788@gmail.com>
2025-06-20 18:58:26 +0530

    pr-3

---
+++ pr-3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './Components/Calcu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
+
